5c8094f5ffe64f5857ab60ca09b778d1e8d39e0c
Rusty Gerard <rusty.gerard@authentect.com>
2016-08-13 16:41:58 -0700

    Initial version of the tiffnamer script

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # tiffnamer
-Extracts tags from TIFF files and renames them with the tag value
+Extracts tags from TIFF files and prepends their filenames with tag's value.
+
+## Command Line Arguments
+
+| Argument | Description | Default Value |
+| -------- | ----------- | ------------- |
+| --image-dir | The directory of images to rename. | './images/' |
+| --dry-run | If true, emit commands that would have been run without making any changes. | 'false' |
+| --tag-index | The integer value of the tag value to fetch from each image. | 65007 |
+
+## Examples
+
+Perform a dry-run:
+
+```
+./tiffnamer.rb --dry-run true
+```
+
+Prepend files using the value of tag index 9999:
+
+```
+./tiffnamer.rb --tag-index 9999
+```
+
+A verbose example of the default behavior:
+
+```
+./tiffnamer.rb --image-dir ./images/ --dry-run false --tag-index 65007
+```
---
+++ tiffnamer.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+#
+# Extracts TIFF image metadata and prepends it to the image's filename
+#
+
+require 'fileutils'
+require 'open3'
+
+ARG_MAP = Hash[*ARGV]
+IMAGE_DIR = ARG_MAP['--image-dir'] || './images/'
+DRY_RUN = (ARG_MAP['--dry-run'] || 'false').downcase == 'true'
+TAG_INDEX = (ARG_MAP['--tag-index'] || '65007').to_i
+
+IMAGE_DIR += File::SEPARATOR unless IMAGE_DIR.end_with?(File::SEPARATOR)
+
+# Test if tiffinfo is available on the path
+raise 'tiffinfo was not found. Please install tiffinfo and ensure it is available on your PATH' unless system('which tiffinfo > /dev/null 2>&1')
+
+raise "image-dir '#{IMAGE_DIR}' was not found." unless Dir.exists?(IMAGE_DIR)
+
+Dir.glob(IMAGE_DIR + '*.tiff') do |filename|
+  basename = File.basename(filename)
+
+  begin
+    puts "INFO: Processing '#{basename}'..."
+
+    stdout, stdeerr, status = Open3.capture3("tiffinfo -r \"#{filename}\"")
+
+    tag_line = stdout.split("\n").select { |x| x.match("^\\s*Tag #{TAG_INDEX}:.*") }.first
+    tag_val = tag_line.gsub("Tag #{TAG_INDEX}:", '').strip rescue nil
+
+    if (tag_val)
+      prefix = tag_val.gsub(/[^\w\.]/, '-')
+      if (File.basename(filename).start_with?(prefix))
+        puts "INFO: Skipping '#{basename}' because it has already been renamed"
+        next
+      end
+
+      new_filename = File.join(File.dirname(filename), prefix + '_' + File.basename(filename))
+      if (DRY_RUN)
+        puts "mv \"#{filename}\" \"#{new_filename}\""
+      else
+        FileUtils.mv(filename, new_filename, {:force => true})
+      end
+    else
+      puts "WARNING: File '#{basename}' does not contain tag #{TAG_INDEX}"
+    end
+  rescue Exception => e
+    puts "ERROR: Failed to process '#{basename}': #{e.message}"
+  end
+end
